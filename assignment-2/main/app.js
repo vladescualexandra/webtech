65937d56b44430e0044e13bce44d4628e95033ba
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-30 15:15:37 +0200

    5/5

--- assignment-2/main/app.js
+++ assignment-2/main/app.js
@@ -1,5 +1,25 @@
 function addTokens(input, tokens){
-    
+    if (typeof(input) != 'string') {
+        throw 'Invalid input';
+    } else {
+        if (input.length < 6) {
+            throw 'Input should have at least 6 characters';
+        } else {
+            for (let item of tokens) {
+                if (!item.hasOwnProperty('tokenName') 
+                        || typeof(item.tokenName) !== 'string' ) {
+                            throw 'Invalid array format';
+                        }
+            } 
+
+            if (input.includes('...')) {
+                for (let token of tokens) {
+                    input = input.replace('...', '${' + token.tokenName + '}');
+                }
+            } 
+            return input;
+        }
+    }
 }
 
 const app = {
