cdd0c61f4b7d5788953bfd889d37fddf152bfad8
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-31 18:05:55 +0200

    done

--- s3-v0/main/src/components/Company.js
+++ s3-v0/main/src/components/Company.js
@@ -1,5 +1,31 @@
 import React, { Component } from 'react'
 
-class Company{}
+class Company extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.item.id,
+            name: this.props.item.name,
+            employees: this.props.item.employees,
+            revenue: this.props.item.revenue
+        }
+
+        this.delete = () => {
+            this.props.onDelete(this.state.id);
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <span> {this.state.id} </span>
+                <span> {this.state.name} </span>
+                <span> {this.state.employees} </span>
+                <span> {this.state.revenue} </span>
+                <input type="button" value="delete" onClick={this.delete}/>
+            </div>
+        )
+    }
+}
 
 export default Company
--- s3-v0/main/src/components/CompanyList.js
+++ s3-v0/main/src/components/CompanyList.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import CompanyStore from '../stores/CompanyStore'
+import Company from './Company';
 
 class CompanyList extends Component {
 	constructor(){
 		super()
 		this.state = {
 			companies : []
+		}
+
+		this.handleDelete = (id) => {
+			this.store = new CompanyStore();
+			this.store.deleteOne(id);
+			this.setState({
+				companies : this.store.getAll()
+			})
+			this.store.emitter.addListener('UPDATE', () => {
+				this.setState({
+					companies : this.store.getAll()
+				})			
+			})
 		}
 	}
 	componentDidMount(){
@@ -22,9 +36,10 @@ class CompanyList extends Component {
   render() {
     return (
       <div>
+		  {this.state.companies.map((el, index) => <Company key={index} item={el} onDelete={this.handleDelete}/>)}
       </div>
     )
   }
 }
 
-export default CompanyList
+export default CompanyList;
