655e0769f96f692858d179b03225cfa21d3de79c
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-31 13:47:10 +0200

    done

--- s1-v2/main/app.js
+++ s1-v2/main/app.js
@@ -1,13 +1,33 @@
 class Duck{
     constructor(name){
-        this.name = name
+        if (typeof(name) === 'string') {
+            this.name = name
+        } else {
+            throw 'name must be string or String';
+        }
     }
+
     move(){
         return `${this.name} is moving`
     }
+
+    swim() {
+        return `${this.name} is swimming`;
+    }
 }
 
-class RubberDuck{
+class RubberDuck extends Duck{
+    constructor(name) {
+        super(name);
+    }
+
+    float() {
+        return `${this.name} floats`;
+    }
+
+    swim() {
+        return `${this.name} can't swim, only float`;
+    }
 }
 
 
