af9d3b59d14cc36c460526817d2dd9d6428abc37
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-31 14:06:10 +0200

    done

--- s1-v4/main/app.js
+++ s1-v4/main/app.js
@@ -1,5 +1,23 @@
 function removeOrderItem(orderInfo, position){
-    
+    if (!Array.isArray(orderInfo.items)) {
+        throw 'Items should be an array';
+    } else {
+        for (let item of orderInfo.items) {
+            if (!item.hasOwnProperty('price')
+                || !item.hasOwnProperty('quantity')) {
+                throw 'Malformed item';
+            }
+        }
+        
+        if (position < 0 || position > orderInfo.items.length) {
+                throw 'Invalid position';
+        } else {
+                orderInfo.total = orderInfo.total 
+                        - orderInfo.items[position].price * orderInfo.items[position].quantity;
+                orderInfo.items.splice(position, 1);
+                return orderInfo;
+        }
+    }
 }
 
 const app = {
