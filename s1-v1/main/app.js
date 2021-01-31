f2a1a908ccb1631d7e1e0f3a6aac5754121cf93b
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-31 13:37:42 +0200

    done

--- s1-v1/main/app.js
+++ s1-v1/main/app.js
@@ -1,4 +1,36 @@
 function calculateFrequencies(input, dictionary){
+    if (typeof(input) !== 'string') {
+        throw 'Input should be a string';
+    } else {
+        if (!Array.isArray(dictionary)) {
+            throw 'Invalid dictionary format';
+        } else {
+            for (let item of dictionary) {
+                if (typeof(item) !== 'string') {
+                    throw 'Invalid dictionary format';
+                }
+            }
+
+            let words = [];
+
+            for (let item of input.split(" ")) {
+                let lc_item = item.toLowerCase();
+                if (!dictionary.includes(lc_item)) {
+                    words.push(lc_item);
+                }
+            }
+
+            let number_of_words = words.length;
+            
+            let result = {};
+            for (let item of words) {
+                let nr = 1 / number_of_words;
+                result[item] = nr;
+            }
+
+            return result;
+        }
+    }
 }
 
 const app = {
