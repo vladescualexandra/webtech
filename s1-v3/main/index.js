9410b2361b4ef93db5a66a2c9d6c82b9b7a0016c
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-01-31 13:57:21 +0200

    done

--- s1-v3/main/index.js
+++ s1-v3/main/index.js
@@ -1,4 +1,24 @@
 class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    insert(element) {
+        if (typeof(element) === 'string') {
+            this.items.push(element);
+        } else {
+            throw 'Invalid Type';
+        }
+    }
+
+    extract() {
+        if (this.items.length === 0) {
+            throw 'Invalid Operation';
+        } else {
+            let item = this.items.shift();
+            return item;
+        }
+    }
   
 }
 
