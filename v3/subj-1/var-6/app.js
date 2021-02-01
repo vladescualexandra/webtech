c37f34c21cb2695990487c27953d21c51e858c7f
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-02-01 18:48:42 +0200

    4/5

--- v3/subj-1/var-6/app.js
+++ v3/subj-1/var-6/app.js
@@ -1,5 +1,34 @@
 function applyBonus(employees, bonus){
     
+    if (typeof(bonus) !== 'number') {
+        // reject cu Error si mesajul Invalid bonus; 
+        return Promise.reject(new Error('Invalid bonus'));
+    } else {
+        let max = 0;
+        for (let item of employees) {
+            if (!item.hasOwnProperty('name')
+                || !item.hasOwnProperty('salary')
+                || typeof(item.name) !== 'string'
+                || typeof(item.salary) !== 'number'
+                ) {
+                    // reject cu Error si mesajul Invalid array format
+                    return Promise.reject(new Error('Invalid array format'));
+                } else if (max < item.salary) {
+                max = item.salary;
+            }
+        }
+
+        
+        if (bonus < (0.1 * max)) {
+            return Promise.reject('Bonus too small');
+        } else {            
+            for (let item of employees) {
+                item.salary = item.salary + bonus;
+            }
+        }
+    }
+    
+    return Promise.resolve(employees);
 }
 
 let app = {
