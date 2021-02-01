bce8433fad6fa7646e6b32082d74fb1495dcbdf0
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-02-01 19:08:08 +0200

    v3/subj-2/var-8 - test 4 is wrong, should be name instead of category :)

--- v3/subj-2/var-8/app.js
+++ v3/subj-2/var-8/app.js
@@ -25,7 +25,47 @@ app.get('/students', (req, res) => {
 });
 
 app.post('/students', (req, res, next) => {
-    res.status(400).json({message: 'Bad request'});
+
+    try {
+
+        if (JSON.stringify(req.body) === JSON.stringify({})) {
+            res.status(500).json({message: 'Body is missing'});
+        } else if (
+            !req.body.hasOwnProperty('name')
+            || !req.body.hasOwnProperty('surname')
+            || !req.body.hasOwnProperty('age')
+            || typeof(req.body.name) !== 'string'
+            || typeof(req.body.surname) !== 'string'
+            || typeof(req.body.age) !== 'number'
+        ) {
+            res.status(500).json({message: 'Invalid body format'});
+        } else if(req.body.age < 0) {
+            res.status(500).json({message: 'Age should be a positive number'});
+        } else {
+            let status = false;
+            for (let item of app.locals.students) {
+                if (item.name === req.body.name) {
+                    status = true;
+                    break;
+                }
+            }
+
+            if (status) {
+                res.status(500).json({message: 'Student already exists'});
+            } else {
+                app.locals.students.push({
+                    name: req.body.name,
+                    surname: req.body.surname,
+                    age: req.body.age
+                });
+                res.status(201).json({message: 'Created'});
+            }
+        }
+
+
+    } catch(err) {
+        res.status(400).json({message: 'Bad request'});
+    }
 })
 
 module.exports = app;
