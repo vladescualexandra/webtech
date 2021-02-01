c554fc42409c3fc6442016b05386503f85d554fd
vladescualexandra <vladescualexandra18@stud.ase.ro>
2021-02-01 20:53:38 +0200

    var-013/subj-2 --- 5/5

--- var-013/subj-2/app.js
+++ var-013/subj-2/app.js
@@ -16,11 +16,44 @@ app.locals.cars = [{
 }]
 
 app.get('/cars', async (req, res) => {
-   // TODO
+    try {
+        if (req.query.filter !== undefined) {
+            let cars = [];
+            for (let item of app.locals.cars) {
+                if (item.brand.includes(req.query.filter)
+                    || item.make.includes(req.query.filter)) {
+                        cars.push(item);
+                    }
+            }
+            res.status(200).json(cars);
+        } else {
+            res.status(200).json(app.locals.cars);
+        }
+    } catch (err) {
+        res.status(500).send({message: 'server error'});
+    }
+
 })
 
 app.post('/cars', async (req, res) => {
-    // TODO
+    try {
+        
+        if (req.body === undefined || JSON.stringify(req.body) === JSON.stringify({})) {
+            res.status(400).json({message: 'body is missing'});
+        } else if (!req.body.hasOwnProperty('brand')
+                    || !req.body.hasOwnProperty('make')
+                    || !req.body.hasOwnProperty('year')) {
+                        res.status(400).send({message: 'malformed request'});
+        } else if (req.body.year < 1860) {
+            res.status(400).send({message: 'year should be > 1860'});
+        } else {
+            app.locals.cars.push(req.body);
+            res.status(201).json({message: 'created'});
+        }
+
+    } catch (err) {
+        res.status(500).send({message: 'server error'});
+    }
 })
 
 module.exports = app
